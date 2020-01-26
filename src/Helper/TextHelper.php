d53cd468f0a228fae0185a729a41381eb464bd4e
Jérémy Halin <jeremy.halin@gmail.com>
2020-01-26 13:00:43 +0100

    feat: add function to TextHelper

--- src/Helper/TextHelper.php
+++ src/Helper/TextHelper.php
@@ -9,13 +9,23 @@ class TextHelper
     /**
      * Search a $needle and replace with $replace in a $text
      *
-     * @param  string $needle
-     * @param  string $replace
-     * @param  string $text
+     * @param  string $needle text to search for
+     * @param  string $replace replacing text
+     * @param  string $text text to search in
      * @return string|string[]
      */
     public static function searchAndReplace(string $needle, string $replace, string $text)
     {
         return str_replace($needle, $replace, $text);
+    }
+
+    /**
+     * Search $needle in $text and return true if $needle is found, false otherwise
+     * @param string $needle text to search for
+     * @param string $text text to search in
+     * @return bool true if $needle is present, false otherwise
+     */
+    public static function doesContain(string $needle, string $text) {
+        return strpos($text, $needle) !== false;
     }
 }
